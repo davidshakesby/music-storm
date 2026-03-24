a212d5eaeb8e7ff88c97bb3943f7f93e5a3611b5
davidshakesby <davidshakesbydev@gmail.com>
2026-03-24 18:11:47 +0000

    Added header to the page.tsx page for placement

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@
 export default function Home() {
   return (
     <main className="">
+      <header className="p-6 border-b">
+        <h2>Music Storm</h2>
+      </header>
       <h1 className="text-3xl">Music Storm</h1>
       <h2>Welcome to the storm</h2>
     </main>
